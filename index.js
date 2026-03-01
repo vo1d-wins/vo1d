8b8e42805c39c6685435f2a9087e8683509612b2
vo1d-wins <youcoreyt@gmail.com>
2026-03-01 00:03:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 import express from 'express';
 import { createServer } from 'node:http';
-import { uvPath } from '@titaniumnetwork-dev/ultraviolet';
 import { join } from 'node:path';
+import { scramjetPath } from '@mercuryworkshop/scramjet';
 
 const app = express();
 const server = createServer(app);
 
 app.use(express.static(join(process.cwd(), 'public')));
-app.use('/uv/', express.static(uvPath));
+app.use('/scramjet/', express.static(scramjetPath));
 
-server.listen(8080, () => {
-  console.log('Server is running on port 8080');
+app.use('/service/', (req, res) => {
+    res.status(200).send(); 
+});
+
+const port = process.env.PORT || 8080;
+
+server.listen(port, () => {
+  console.log(`vo!d online ! port: ${port}`);
 });
